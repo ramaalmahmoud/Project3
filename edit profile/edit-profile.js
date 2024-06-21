e34e42a226dcbb2ba2c9cc39c2a8050fcf19b03b
ramaalmahmoud <ramaoudat151@gmail.com>
2024-06-21 15:31:29 +0300

    problems in navbar and footer

--- edit profile/edit-profile.js
+++ edit profile/edit-profile.js
@@ -1,4 +1,5 @@
 
+
 function display(){
   let profileImg=document.getElementById('profileImg');
     const storedImage=JSON.parse(localStorage.getItem('hrJson')).Image;
@@ -21,29 +22,15 @@ function display(){
    const storedEmail=JSON.parse(localStorage.getItem('hrJson')).email;
    email.value=storedEmail;
 
+   let bio =document.getElementById('bio');
+   const storedBio=localStorage.getItem('bio');
+   bio.textContent=storedBio;
+
 
 
 }
 display();
-// current pass
-// let storedPassword=JSON.parse(localStorage.getItem('hrJson')).password;
-// console.log("cp",storedPassword);
 
-// document.getElementById('currentPassword').addEventListener('input',function(){
-//   const currentPassword=document.getElementById('currentPassword').value;
-//   console.log(currentPassword);
-//   if (currentPassword!==storedPassword && currentPassword!==''){
-//     document.getElementById('error').textContent='the current password is wrong';
-//     return;
-    
-//     }
-
-//   else
-//   document.getElementById('error').textContent='';
-
-    
-
-// });
 
 // remove picture
 
@@ -93,110 +80,113 @@ rmv_img.addEventListener('click',function(e){
  });
 });
 
+function validateEmail() {
+  let EmailInput = document.getElementById("Email");
+  const Email = EmailInput.value;
+  const EmailPat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (Email === "" || !EmailPat.test(Email)) {
+      document.getElementById("errorEmail").textContent = "Invalid email address";
+      return false;
+  } else {
+      document.getElementById("errorEmail").textContent = "";
+      return true;
+  }
+}
+function validateCurrentPassword() {
+  const currentPassword = document.getElementById('currentPassword').value;
+  const storedPassword = JSON.parse(localStorage.getItem('hrJson')).password;
+
+  if (currentPassword !== storedPassword && currentPassword !== '') {
+      document.getElementById('error').textContent = 'The current password is wrong';
+      return false;
+  } else {
+      document.getElementById('error').textContent = '';
+      return true;
+  }
+}
 
 
+function validateNewPassword() {
+  const newPassword = document.getElementById('newPassword').value;
+  const renewPassword = document.getElementById('renewPassword').value;
+  const passPat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+  if (newPassword !== "" && !passPat.test(newPassword)) {
+      document.getElementById("error1").textContent = "Invalid password. Password must be at least 8 characters long and contain at least one letter and one number";
+      return false;
+  } else {
+      document.getElementById("error1").textContent = "";
+
+      if (newPassword === renewPassword) {
+          document.getElementById("error2").textContent = "";
+          return true;
+      } else {
+          document.getElementById("error2").textContent = "Passwords do not match";
+          return false;
+      }
+  }
+}
 
 function onInput() {
   let isValid = true;
 
-  // fullname
-  let nameInput = document.getElementById("fullName");
-  const name = nameInput.value;
-  localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), full_name: name }));
-
-
-  // possition
-  let JobInput = document.getElementById("Job");
-  const Job = JobInput.value;
-  localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), Postion: Job }));
-
-
-  // phone
-  let PhoneInput = document.getElementById("Phone");
-  const Phone = PhoneInput.value;
-  localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), Phone: Phone }));
-
-
-  // email
-  let EmailInput = document.getElementById("Email");
-  const Email = EmailInput.value;
-  const EmailPat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   
-  if (Email === "" || !EmailPat.test(Email)) {
-    document.getElementById("errorEmail").innerHTML = "Invalid email address";
-    isValid = false;
-  } else {
-    document.getElementById("errorEmail").innerHTML = "";
-    localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), email: Email }));
-  }
+ 
+
+  isValid = validateEmail() && validateCurrentPassword() && validateNewPassword();
+ 
 
 
 
-
-  // current pass
-let storedPassword=JSON.parse(localStorage.getItem('hrJson')).password;
-document.getElementById('currentPassword').addEventListener('input',function(){
-  const currentPassword=document.getElementById('currentPassword').value;
-  console.log(currentPassword);
-  if (currentPassword!=storedPassword && currentPassword!=''){
-    document.getElementById('error').textContent='the current password is wrong';
-    isValid = false;
-    return;
-    
-    }
-
-  else
-  document.getElementById('error').textContent='';
-
-    
-
-});
-
-
-
-  // new password
-  let newPassword = document.getElementById('newPassword').value;
-  let renewPassword = document.getElementById('renewPassword').value;
-
-  const passPat = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-
-  if (newPassword !== "" && !passPat.test(newPassword)) {
-    document.getElementById("error1").innerHTML = "Invalid password. Password must be at least 8 characters long and contain at least one letter and one number";
-    isValid = false;
-    return;
-  } else {
-    document.getElementById("error1").innerHTML = "";
-    if (newPassword === renewPassword) {
-      localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), password: renewPassword }));
-    } else {
-      document.getElementById("error2").innerHTML = "Passwords do not match";
-      isValid = false;
-      return;
-    }
-  }
-
+  
   return isValid;
 }
+
+document.getElementById("Email").addEventListener('input', validateEmail);
+document.getElementById("currentPassword").addEventListener('input', validateCurrentPassword);
+document.getElementById("newPassword").addEventListener('input', validateNewPassword);
+document.getElementById("renewPassword").addEventListener('input', validateNewPassword);
 let saveButt = document.getElementById('saveButt');
-saveButt.addEventListener("click", function(event) {
+
+saveButt.addEventListener('click', function(event) {
   event.preventDefault();
-  if (onInput()) {
-    let profileImgSrc = document.getElementById('profileImg').src;
-    let fullNameValue = document.getElementById('fullName').value;
-    let jobValue = document.getElementById('Job').value;
-    localStorage.setItem('hrJson', JSON.stringify({
-      ...JSON.parse(localStorage.getItem('hrJson')),
-      Image: profileImgSrc,
-      full_name: fullNameValue,
-      Postion: jobValue,
-      
+  if (!onInput()) { return;}
+   
 
-    }));
-    window.location.href = '../profile/profile.html'; 
-    alert('Profile updated successfully!');
-  }
+
+//  profile image
+let profileImgSrc = document.getElementById('profileImg').src;
+localStorage.setItem('hrJson', JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), Image: profileImgSrc }));
+
+//  full name
+let nameInput = document.getElementById('fullName').value;
+localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), full_name: nameInput }));
+
+//  position
+let jobInput = document.getElementById('Job').value;
+localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), Position: jobInput }));
+
+
+//bio
+let bio=document.getElementById('bio').value;
+localStorage.setItem('bio',bio);
+//  phone
+let phoneInput = document.getElementById('Phone').value;
+localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), Phone: phoneInput }));
+
+// email
+let emailInput = document.getElementById('Email').value;
+localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), email: emailInput }));
+
+// password
+let  renewPassword=document.getElementById('renewPassword').value;
+if (renewPassword !== "") {
+  localStorage.setItem("hrJson", JSON.stringify({ ...JSON.parse(localStorage.getItem('hrJson')), password: renewPassword }));
+}
+
+window.location.href = '../profile/profile2.html';
+alert('Profile updated successfully!');
+  
 });
-
-
-
 
--- profile/profile.js
+++ profile/profile.js
@@ -1,9 +1,12 @@
+ 
+ 
  async function checkIfINLocal() {
-   if (!localStorage.getItem('hrJson')) {
+   if (!localStorage.getItem('hrJson')||!localStorage.getItem('bio')) {
      try {
        let response = await fetch('HR-info.json');
        let hrJson = await response.json();
        localStorage.setItem('hrJson', JSON.stringify(hrJson));
+       localStorage.setItem("bio","P.S I have no special talent, I'm just passionately curious ;)");
      } catch (error) {
        console.error('Failed to fetch HR-info.json:', error);
      }
@@ -15,6 +18,7 @@
  async function display() {
    await checkIfINLocal(); 
    try {
+    const storedBio=localStorage.getItem('bio');
      const storedData = JSON.parse(localStorage.getItem('hrJson'));
      let image = document.getElementById('image');
      let name = document.getElementById('name');
@@ -22,7 +26,8 @@
      let position = document.getElementById('position');
      let phone = document.getElementById('phone');
      let email = document.getElementById('email');
- 
+     let bio=document.getElementById('bio');
+ bio.textContent=storedBio;
      image.src = storedData.Image;
      name.textContent = storedData.full_name;
      Id.textContent = storedData.ID;
